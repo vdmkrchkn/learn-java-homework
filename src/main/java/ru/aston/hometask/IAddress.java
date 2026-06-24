07f7f0f5e677fcadfa3cb7320f02eb4d223ddcfc
vadim <vdmkrchkn@gmail.com>
2026-06-24 16:39:11 +0300

    add maven 4 unittest

--- src/main/java/ru/aston/hometask/IAddress.java
+++ src/main/java/ru/aston/hometask/IAddress.java
@@ -1,5 +1,3 @@
-package ru.aston.hometask;
-
 public interface IAddress extends Cloneable {
     String getCity();
     void setCity(String city);
---
+++ src/test/java/UserTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTests {
+    @Test
+    void userAddressNotChanged(){
+        String city = "city";
+        User user = new User("name", new Address(city));
+        IAddress newAddress = user.getAddress();
+        newAddress.setCity("Moscow");
+        assertEquals(city, user.getAddress().getCity());
+    }
+}
